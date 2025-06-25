21951bd4684f3987bf24e91eb2516d536e0751bf
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-06-25 07:43:04 -0300

    Criada estrutura Matematica e funcões matemáticas simples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use crate::matematica::Quadratica;
+
+mod matematica;
+
 fn main() {
-    println!("Hello, world!");
+    // testes na main
+    let equacao = Quadratica::new(1.0, 2.0, 5.0);
+    let x = 1.0;
+    
+    println!("{}", equacao.calcular_valor(x));
+    println!("{:?}", equacao.calcular_raizes());
+    println!("{:?}", equacao.calcular_vertice())
 }
---
+++ src/matematica.rs
@@ -0,0 +1,52 @@
+// criando a estrutura onde se enquadrarão equações quadráticas
+pub struct Quadratica{
+    a: f64,
+    b: f64,
+    c: f64,
+}
+
+impl Quadratica{
+    pub fn new(a: f64, b: f64, c: f64) -> Quadratica{
+        Quadratica{a, b, c}
+    }
+    
+    // recebe valor x e calcula f(x)
+    pub fn calcular_valor(&self, x: f64) -> f64{
+        self.a * x.powi(2) + self.b * x + self.c
+    }
+    
+    // calcula o delta da função
+    pub fn calcular_delta(&self) -> f64{
+        self.b.powi(2) - 4.0 * self.a * self.c
+    }
+    
+    // calculas as raízes da função
+    pub fn calcular_raizes(&self) -> Vec<f64>{
+        let mut raizes: Vec<f64> = Vec::new();
+        let delta: f64 = self.calcular_delta();
+
+        match delta{
+            d if d >= 0.0 => {
+                raizes.push((-self.b + delta.sqrt()) / (2.0 * self.a));
+                raizes.push((-self.b - delta.sqrt()) / (2.0 * self.a));
+            }
+
+            _ => ()
+        }
+
+        if raizes.len() == 2 && raizes[0] == raizes[1]{
+            raizes.pop();      
+        }
+
+        raizes
+    }
+    
+    // calcula o vértice da função
+    pub fn calcular_vertice(&self) -> Vec<f64>{
+        let mut vertice: Vec<f64> = Vec::new();
+        
+        vertice.push(-self.b / (2.0 * self.a));
+        vertice.push(-self.calcular_delta() / (4.0 * self.a));
+        vertice
+    }
+}
