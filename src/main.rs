4fa62fc65643918f04fa03498710bb94d221a72d
Kevin de Oliveira <kevinmsoliveira@gmail.com>
2025-06-26 08:53:42 -0300

    Criado método calcular_intercepto_y que retorna ponto onde X = 0

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ mod matematica;
 fn main() {
     // testes na main
     let equacao = Quadratica::new(1.0, 2.0, 5.0);
-    let x = 1.0;
     
+    let x = 1.0;
+
     println!("{}", equacao.calcular_valor(x));
     println!("{:?}", equacao.calcular_raizes());
+    println!("{:?}", equacao.calcular_intercepto_y());
     println!("{:?}", equacao.calcular_vertice());
-    println!("{:?}", equacao.gerar_pontos(0.0..5.0, 500));
+    println!("{:?}", equacao.gerar_pontos(0.0..5.0, 5));
 }
--- src/matematica.rs
+++ src/matematica.rs
@@ -11,7 +11,7 @@ impl Quadratica{
     pub fn new(a: f64, b: f64, c: f64) -> Quadratica{
         Quadratica{a, b, c}
     }
-    
+
     // recebe valor x e calcula f(x)
     pub fn calcular_valor(&self, x: f64) -> f64{
         self.a * x.powi(2) + self.b * x + self.c
@@ -38,10 +38,15 @@ impl Quadratica{
         }
 
         if raizes.len() == 2 && raizes[0] == raizes[1]{
-            raizes.pop();      
+            raizes.pop();
         }
 
         raizes
+    }
+    
+    // calcula ponto onde a parábola intercepta o eixo y
+    pub fn calcular_intercepto_y(&self) -> (f64, f64){
+        (0.0, self.calcular_valor(0.0))
     }
 
     // calcula o vértice da função
@@ -50,17 +55,17 @@ impl Quadratica{
         let y = -self.calcular_delta() / (4.0 * self.a);
         (x, y)
     }
-    
+
     // método que calcula f(x) para todos os valores de x necessários
     pub fn gerar_pontos(&self, intervalo: Range<f64>, passos: usize) -> Vec<(f64, f64)>{
         let mut pontos: Vec<(f64, f64)> = Vec::new();
         let passo = (intervalo.end - intervalo.start) / passos as f64;
-        
+
         for i in 0..=passos{
             let x = intervalo.start + i as f64 * passo;
             pontos.push((x, self.calcular_valor(x)));
         }
-        
+
         pontos
     }
 }
